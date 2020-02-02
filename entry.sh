3b0dc38ce4c4472a3489219c418aa8d4044a9807
NickPaul41 <442364+NickPaul41@users.noreply.github.com>
2020-02-01 23:39:30 -0600

    Removed the code that required distributing zap2xml.pl.

--- entry.sh
+++ entry.sh
@@ -3,10 +3,18 @@
 # start cron
 #/usr/sbin/crond -f -l 8
 
+FILE=/config/zap2xml.pl
+while [ ! -f "$FILE" ]
+do
+    echo "File not found: $FILE"
+    sleep 5
+done
+
+
 while :
 do
 	DATE=$(date)
-	/zap2xml.pl -u "$USERNAME" -p "$PASSWORD" -U -o /data/"$XMLTV_FILENAME" "$OPT_ARGS"
+	/config/zap2xml.pl -u "$USERNAME" -p "$PASSWORD" -U -o /data/"$XMLTV_FILENAME" "$OPT_ARGS"
 	echo "Last run time: $DATE"
 	echo "Will run in $SLEEPTIME seconds"
 	sleep "$SLEEPTIME"
