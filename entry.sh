999e12fe8d204d7003ba6744fa0230fe684c9c64
NickPaul41 <442364+NickPaul41@users.noreply.github.com>
2020-02-03 11:27:40 -0600

    $OPT_ARGS should not be double quoted as that will not allow multiple commands to be sent.

--- entry.sh
+++ entry.sh
@@ -14,7 +14,8 @@ done
 while :
 do
 	DATE=$(date)
-	/config/zap2xml.pl -u "$USERNAME" -p "$PASSWORD" -U -o /data/"$XMLTV_FILENAME" "$OPT_ARGS"
+	# shellcheck disable=SC2086
+	/config/zap2xml.pl -u "$USERNAME" -p "$PASSWORD" -U -o /data/"$XMLTV_FILENAME" $OPT_ARGS
 	echo "Last run time: $DATE"
 	echo "Will run in $SLEEPTIME seconds"
 	sleep "$SLEEPTIME"
